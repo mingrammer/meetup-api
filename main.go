de88313691b55e085cc5be2ef5ce6547540a8252
mingrammer <k239507@gmail.com>
2017-08-18 09:01:44 +0900

    Support HTTPS

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ func main() {
 
 	go func() {
 		fmt.Printf("The meetup web server is running on :%d\n", config.WebAPIConfig.Port)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.WebAPIConfig.Port), webAPI.MakeHandler()))
+		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", config.WebAPIConfig.Port), "certificate.crt", "private.key", webAPI.MakeHandler()))
 	}()
 
 	go func() {
 		fmt.Printf("The meetup bot server is running on :%d\n", config.BotAPIConfig.Port)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.BotAPIConfig.Port), botAPI.MakeHandler()))
+		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%d", config.BotAPIConfig.Port), "certificate.crt", "private.key", botAPI.MakeHandler()))
 	}()
 
 	<-stop
